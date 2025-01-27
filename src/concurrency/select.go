3b7475d22f85a3912319a1587a5134c3715b99c6
Brian Travis <bztravis88@gmail.com>
2025-01-27 15:24:48 -0500

    Add select

---
+++ src/concurrency/select.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		default:
+			fmt.Println("waiting...")
+		}
+	}
+}
+
+// select statement runs when one of the cases can continue
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
+}
