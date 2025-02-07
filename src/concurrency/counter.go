b7928a01788c75080f54a518fdf4e665ced81e48
Brian Travis <bztravis88@gmail.com>
2025-02-06 22:03:29 -0500

    Add channel pass to count, do all work, pass back to channel for mut ex, but deadlock at end
    last counter puts val back into rendezvous channel but main has to wait before taking out again

--- src/concurrency/counter.go
+++ src/concurrency/counter.go
@@ -6,29 +6,36 @@ import (
 	"sync"
 )
 
-func count(val *int, label string) {
-	fmt.Printf("%v init val %v\n", label, *val)
+func count(c chan int, label string) {
+	val := <-c
+
+	fmt.Printf("%v init val %v\n", label, val)
 
 	for i := 0; i < 1000000; i++ {
-		*val = *val + 1
+		val++
 	}
 
-	fmt.Printf("%v final val %v\n", label, *val)
+	fmt.Printf("%v final val %v\n", label, val)
+
+	c <- val
 }
 
 func main() {
 	val, num := 0, 4
 	var wg sync.WaitGroup
+	c := make(chan int)
 
 	wg.Add(num)
 	for i := 0; i < num; i++ {
 		go func(i int) { // pass in the value to be captured as an argument, since it's not guaranteed to be the same value when the goroutine runs. This is fixed since Go v.1.22 https://tip.golang.org/doc/go1.22
 			defer wg.Done()
-			count(&val, "counter"+strconv.Itoa(i))
+			count(c, "counter"+strconv.Itoa(i))
 		}(i)
 	}
 
+	c <- val
 	wg.Wait()
+	val = <-c
 
 	fmt.Printf("Done! val is %v\n", val)
 }
